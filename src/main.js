e01351b49f9879ccffe050b96ab43777c63352e3
jinju0228 <jin_ju0228@daum.net>
2022-08-09 09:33:55 +0900

    firebase

--- src/main.js
+++ src/main.js
@@ -9,6 +9,24 @@ import '/public/css/project.css'
 import '/public/css/contact.css'
 import '/public/js/common.js'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4dqbpliSnJPX5sh-fhjLgN64x5dGWXqk",
+  authDomain: "j-jportfolio.firebaseapp.com",
+  databaseURL: "https://j-jportfolio-default-rtdb.firebaseio.com",
+  projectId: "j-jportfolio",
+  storageBucket: "j-jportfolio.appspot.com",
+  messagingSenderId: "903858117648",
+  appId: "1:903858117648:web:75b72b63cdb7844e477e95"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 
 const app = createApp(App)
--- src/views/about_page.vue
+++ src/views/about_page.vue
@@ -90,7 +90,7 @@ export default {
   data:function() {
 return {
    myinfo:['1996.02.28 Yangpyeong','010-8758-9982','jin_ju28@daum.net','성신여자대학교, 청정융합과학과'],
-  education:[{date:'2022.02.22 ~ 08.16',subject:'[디지털컨버전스] 스마트_뷰(Vue)활용프론트엔드 웹개발자 양성과정', place:'노원 그린 컴퓨터',complete:'수료'},{date:'2021.12.02 ~ 12.30',subject:'[시각디자인] 출판편집(포토샵, 일러스트)',place:'종로 그린 컴퓨터',complete:'수료'}],
+  education:[{date:'2022.02.22 ~ 08.16',subject:'[디지털컨버전스] 스마트_뷰(Vue)활용프론트엔드 웹개발자 양성과정', place:'노원 그린 컴퓨터',complete:'수료 예정'},{date:'2021.12.02 ~ 12.30',subject:'[시각디자인] 출판편집(포토샵, 일러스트)',place:'종로 그린 컴퓨터',complete:'수료'}],
   certificate:[{date:'2020.07',cerName:'컴퓨터활용능력 1급'},{date:'2022.01',cerName:'GTQ 1급'}],
   tool:[{toolUrl: "../img/1.png"},{toolUrl: "../img/2.png"},{toolUrl: "../img/3.png"},{toolUrl: "../img/4.png"},{toolUrl: "../img/5.png"},{toolUrl: "../img/6.png"},{toolUrl: "../img/7.png"},{toolUrl: "../img/8.png"},{toolUrl: "../img/9.png"},{toolUrl: "../img/firebase.png"},{toolUrl: "../img/10.png"}],  
   skill:[{skillUrl: "../img/icon1.png"},{skillUrl: "../img/icon2.png"},{skillUrl: "../img/icon3.webp"},{skillUrl: "../img/icon4.png"},{skillUrl: "../img/icon5.png"},{skillUrl: "../img/icon6.png"},{skillUrl: "../img/icon7.png"}],
